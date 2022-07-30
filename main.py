a4b6a3e7124b2d6ca304a452b9a31d6e5299bb4f
alexander-volodi <alexandervolodin2020@gmail.com>
2022-07-30 10:10:03 +0300

    Program can choose true fifteens

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ colors = [(190, 170, 210), (160, 220, 220), (220, 220, 160), (210, 150, 150), (1
 colors_for_rects = dict(zip(range(1, 16), colors))
 fifteens_table = [[0] * 4 for i in range(4)]
 indexes = []
+total_table = [[i + j * 4 + 1 for j in range(4)] for i in range(4)]
+total_table[3][3] = 0
+
 for i in range(1, 16):
     coordinate = randint(0, 15)
     indexes.append(i)
@@ -24,6 +27,35 @@ for i in range(4):
             fifteens_table[i][j] = coordinates[i * 4 + j]
         else:
             fifteens_table[i][j] = 0
+total = 0
+for i in range(4):
+    for j in range(4):
+        for k in range(i, 4):
+            for m in range(j, 4):
+                if fifteens_table[i][j] > fifteens_table[k][m]:
+                    total += 1
+impossible = total % 2
+while impossible:
+    for i in range(1, 16):
+        coordinate = randint(0, 15)
+        indexes.append(i)
+        while coordinate in coordinates:
+            coordinate = randint(0, 15)
+        coordinates[coordinate] = i
+    for i in range(4):
+        for j in range(4):
+            if i * 4 + j in coordinates:
+                fifteens_table[i][j] = coordinates[i * 4 + j]
+            else:
+                fifteens_table[i][j] = 0
+    total = 0
+    for i in range(4):
+        for j in range(4):
+            for k in range(i, 4):
+                for m in range(j, 4):
+                    if fifteens_table[i][j] > fifteens_table[k][m]:
+                        total += 1
+    impossible = total % 2
 
 fifteens_surf = pg.display.set_mode((400, 400))
 fifteens_surf.fill(BACK_GROUND)
@@ -68,6 +100,23 @@ while True:
                         fifteens_table[coordinate_mouse_x][coordinate_mouse_y], fifteens_table[coordinate_mouse_x][coordinate_mouse_y + 1] = \
                         fifteens_table[coordinate_mouse_x][coordinate_mouse_y + 1], fifteens_table[coordinate_mouse_x][coordinate_mouse_y]
             fifteens_surf.fill(BACK_GROUND)
+            for i in range(4):
+                for j in range(4):
+                    if fifteens_table[i][j]:
+                        pg.draw.rect(fifteens_surf, colors_for_rects[fifteens_table[i][j]],
+                                     (i * 100, j * 100, 100, 100),
+                                     border_radius=10)
+                        numbers_text = numbers_font.render(str(fifteens_table[i][j]), True,
+                                                           (40, 40, 40))
+                        fifteens_surf.blit(numbers_text, (i * 100 + 40, j * 100 + 40))
+            pg.display.update()
+
+        if fifteens_table == total_table:
+            won_font = pg.font.Font(None, 70)
+            won_text = won_font.render("You win!", True, (40, 40, 40))
+            fifteens_surf = pg.display.set_mode((400, 500))
+            fifteens_surf.fill(BACK_GROUND)
+            fifteens_surf.blit(won_text, (100, 425))
             for i in range(4):
                 for j in range(4):
                     if fifteens_table[i][j]:
